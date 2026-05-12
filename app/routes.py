675f437d231e5dbfe5f263aa3a5200758e9dda8a
Itsmerepository <febriannaufalirfansyah@gmail.com>
2026-05-12 12:59:39 +0700

    still trying on day 5

--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,7 @@ from app import db
 from app.models import User
 from app import app
 from app.forms import LoginForm, RegistrationForm
+from datetime import datetime, timezone
 
 @app.route('/')
 @app.route('/index')
@@ -59,3 +60,19 @@ def register():
         flash('Sudah teregistrasi selamat bersenang-senangggg!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = db.first_or_404(sa.select(User).where(User.username == username))
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.now(timezone.utc)
+        db.session.commit()
