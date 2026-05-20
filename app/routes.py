b8bd484e5d5e9ab1ed3bb9bed24741eba5c1720c
Ini Irfan <febriannaufalirfansyah@gmail.com>
2026-05-20 23:22:06 +0700

    late commit

--- app/routes.py
+++ app/routes.py
@@ -3,26 +3,24 @@ from flask_login import current_user, login_user, logout_user, login_required
 from urllib.parse import urlsplit
 import sqlalchemy as sa
 from app import db
-from app.models import User
+from app.models import User, Post
 from app import app
-from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm, PostForm
 from datetime import datetime, timezone
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-    posts = [
-        {
-            'author' : {'username' : 'Hern'},
-            'body' : 'Dia lagi ngapain ya?'
-        },
-        {
-            'author' : {'username' : 'Fans'},
-            'body' : 'Di dump lagi ngga ya?'
-        }
-    ]
-    return render_template('index.html', title='Home Page', posts = posts)
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
+    posts = db.session.scalars(current_user.following_posts()).all()
+    return render_template('index.html', title='Home Page', form=form, posts = posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -134,3 +132,9 @@ def unfollow(username):
     else:
         return redirect(url_for('index'))
         
+@app.route('/explore')
+@login_required
+def explore():
+    query = sa.select(Post).order_by(Post.timestamp.desc())
+    posts = db.session.scalars(query).all()
+    return render_template('index.html', title='Explore', posts=posts)
