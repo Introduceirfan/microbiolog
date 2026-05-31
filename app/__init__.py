90e1bfc33fadfa85391d7419e84f29883e135707
Ini Irfan <febriannaufalirfansyah@gmail.com>
2026-05-31 20:47:48 +0700

    adding language

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+from flask import request
 import logging
 from logging.handlers import RotatingFileHandler, SMTPHandler
 import os
@@ -8,7 +9,11 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_moment import Moment
+from flask_babel import Babel
 
+def get_locale():
+    return request.accept_languages.best_match(app.config['Languages'])
+    
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
@@ -17,6 +22,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 moment = Moment(app)
+babel = Babel(app, locale_selector = get_locale)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
